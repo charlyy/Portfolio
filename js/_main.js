1d55af7cfa3da04e1f70d8f45985c2566cdb3e11
charlyy <cyoon90@yahoo.com>
2014-07-14 01:06:36 -0700

    added foundation, contact modal and navbar

--- js/_main.js
+++ js/_main.js
@@ -39,3 +39,17 @@
 	    s.refresh($('.homeSlide'));
 	}		
 })(jQuery);
+
+// scroll to section effect
+
+$('a[href^="#"]').bind('click.smoothscroll', function(e){
+  e.preventDefault();
+
+  var target = $(this).hash,
+  $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop' : $target.offset().top
+    }, 900, 'swing', function(){
+      window.location.hash = target;
+  });
+});
